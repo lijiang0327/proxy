79b18d2745d8aec9279d83e6b3a9cb333851fc78
lijiang <lijiang0327@126.com>
2016-10-24 17:39:06 +0800

    init

--- router.js
+++ router.js
@@ -12,7 +12,7 @@ router.get('/', (req, res) => {
     //     res.setHeader('contentType', 'text/html; charset=UTF-8');
     //     res.status(resp.statusCode).end(body);
     // });
-    request.get('https://www.baidu.com').pipe(res);
+    request.get('https://www.google.com').pipe(res);
 });
 
 module.exports = router;
