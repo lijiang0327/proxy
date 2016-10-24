851041fa1c722ac1822df4606932521f2e6c63d1
lijiang <lijiang0327@126.com>
2016-10-24 17:37:29 +0800

    init

--- router.js
+++ router.js
@@ -6,10 +6,13 @@ var request = require('request');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    request('https://www.google.com', (err, resp, body) => {
-        res.setHeader('contentType', 'text/html; charset=UTF-8');
-        res.status(200).end(body);
-    });
+    // request('https://www.baidu.com', (err, resp, body) => {
+    //     console.log(resp);
+    //     console.log(resp._multipart.getHeader());
+    //     res.setHeader('contentType', 'text/html; charset=UTF-8');
+    //     res.status(resp.statusCode).end(body);
+    // });
+    request.get('https://www.baidu.com').pipe(res);
 });
 
 module.exports = router;
